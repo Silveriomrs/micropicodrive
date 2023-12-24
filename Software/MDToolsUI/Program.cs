e92728b068875c831adc46b73d5e40a92ebfa985
Silveriomrs <silverio_m@hotmail.com>
2023-12-24 20:14:45 +0100

    first commit

--- Software/MDToolsUI/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MDToolsUI;
-using MicroDriveTools.Classes;
-using System.Xml.Serialization;
-using Terminal.Gui;
-
-Application.Run<DirectoryExplorer>();
-Application.Shutdown();
